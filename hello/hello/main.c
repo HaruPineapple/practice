e0e78a2aeaa0390b3a3bf1d253ff5db23fb4e2ce
HaruPineapple <happyharu@163.com>
2022-09-06 18:20:09 +0800

    初试数组

--- hello/hello/main.c
+++ hello/hello/main.c
@@ -6,22 +6,27 @@
 //
 
 #include <stdio.h>
-//会交换a和b的值吗？
-void swap(int a,int b);
+//求多值的平均数，并输出比平均数大的值
 int main(){
-    int a=5;
-    int b=6;
-    swap(a,b);
-    printf("a=%d,b=%d\n",a,b);
+    int x;
+    double sum=0;
+    int cnt=0;
+    int number[100];//定义了一个变量，这个变量是数组。里面每个单元是int，数组大小是100=可以放100个int
+    scanf("%d",&x);
+    while(x!=-1){
+        number[cnt]=x;
+        sum+=x;
+        cnt++;
+        scanf("%d",&x);
+    }
+    if(cnt>0){
+        printf("%f\n",sum/cnt);
+        int i;
+        for (i=0;i<cnt;i++){
+            if(number[i]>sum/cnt){
+                printf("%d\n",number[i]);
+            }
+        }
+    }
     return 0;
 }
-void swap(int a,int b){
-    int t=a;
-    a=b;
-    b=t;
-}
-//不会交换a和b的值：C语言在调用函数时，只能传值给函数
-//  过去                    =>     现在
-//----------------------------------------
-//  形式参数form parameter   =>     参数
-//  实际参数real parameter   =>     值
