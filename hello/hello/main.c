c35e81b2c1ce36e1d3c7dbb623030db4076b4a7d
HaruPineapple <happyharu@163.com>
2022-09-06 16:48:21 +0800

    参数传递

--- hello/hello/main.c
+++ hello/hello/main.c
@@ -6,9 +6,22 @@
 //
 
 #include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+//会交换a和b的值吗？
+void swap(int a,int b);
+int main(){
+    int a=5;
+    int b=6;
+    swap(a,b);
+    printf("a=%d,b=%d\n",a,b);
     return 0;
 }
+void swap(int a,int b){
+    int t=a;
+    a=b;
+    b=t;
+}
+//不会交换a和b的值：C语言在调用函数时，只能传值给函数
+//  过去                    =>     现在
+//----------------------------------------
+//  形式参数form parameter   =>     参数
+//  实际参数real parameter   =>     值
