82f251c06b9159d0ed68febe18237d393e57c21d
HaruPineapple <happyharu@163.com>
2022-09-06 15:25:14 +0800

    init hello

---
+++ hello/hello/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  hello
+//
+//  Created by Haru on 2022/9/6.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
