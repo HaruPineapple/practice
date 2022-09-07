db4b6a67a42db523a3cbfe77d53298d2797ab11e
HaruPineapple <happyharu@163.com>
2022-09-07 17:42:13 +0800

    数组运算：search

--- hello/hello/main.c
+++ hello/hello/main.c
@@ -6,25 +6,48 @@
 //
 
 #include <stdio.h>
-//用数组统计数的个数
-int main(){
-    
-    const int number=10;//觉得数组大小
-    int x;
-    int count[number];//定义数组
-    int i;
-    for(i=0;i<number;i++){
-        count[i]=0;
-    }//初始化数组
-    scanf("%d",&x);
-    while(x!=-1){
-        if (x>=0 && x<=9){
-            count[x] ++;//数组参与运算
+
+/**
+ 找出key在数组a中的位置
+ @param key 要寻找的数字
+ @param a   要寻找的数组
+ @param length  数组a的长度
+ @return 如果找到，返回其在a中的位置；如果找不到则返回-1
+ */
+int search(int key, int a[],int length);
+int main(void){
+    int a[]={2,4,6,7,1,3,5,9,11,13,23,14,32};
+    {
+        int i;
+        printf("%lu\n",sizeof(a));
+        printf("%lu\n",sizeof(a[0]));
+        for(i=0;i<sizeof(a)/sizeof(a[0]);i++){
+            printf("%d\t",a[i]);
         }
-        scanf("%d",&x);
+        printf("\n");
     }
-    for(i=0;i<number;i++){
-        printf("%d:%d\n",i,count[i]);
-    }//遍历数组输出
+    
+    int x;
+    int loc;
+    printf("Give me a number: ");
+    scanf("%d",&x);
+    loc=search(x,a,sizeof(a)/sizeof(a[0]));
+    if(loc!=-1){
+        printf("%d is on %d order\n",x,loc+1);
+    }else{
+        printf("%d not exit\n",x);
+    }
     return 0;
+    
+}
+int search(int key,int a[],int length){
+    int ret=-1;
+    int i;
+    for(i=0;i<length;i++){
+        if(a[i]==key){
+            ret =i;
+            break;
+        }
+    }
+    return ret;
 }
