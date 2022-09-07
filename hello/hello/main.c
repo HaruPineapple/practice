0036f1bd84392aa02c7f506b387cd16ea7fd9fb1
HaruPineapple <happyharu@163.com>
2022-09-07 18:13:49 +0800

    数组例子：素数

--- hello/hello/main.c
+++ hello/hello/main.c
@@ -4,48 +4,45 @@
 //
 //  Created by Haru on 2022/9/6.
 //
-
+//数组例子：素数
 #include <stdio.h>
 
-/**
- 找出key在数组a中的位置
- @param key 要寻找的数字
- @param a   要寻找的数组
- @param length  数组a的长度
- @return 如果找到，返回其在a中的位置；如果找不到则返回-1
- */
-int search(int key, int a[],int length);
+int isPrime(int x,int knownPrimes[],int numberOfKnownPrimes);
+
 int main(void){
-    int a[]={2,4,6,7,1,3,5,9,11,13,23,14,32};
-    {
-        int i;
-        printf("%lu\n",sizeof(a));
-        printf("%lu\n",sizeof(a[0]));
-        for(i=0;i<sizeof(a)/sizeof(a[0]);i++){
-            printf("%d\t",a[i]);
+    const int number =100;
+    int prime[number]={2};
+    int count=1;
+    int i=3;
+    while(count <number){
+        if(isPrime(i, prime, count)){
+            prime[count++]=i;//做了两件事：1.把i放在count=1的位置；2.cpunt++。
         }
-        printf("\n");
+        i++;
     }
-    
-    int x;
-    int loc;
-    printf("Give me a number: ");
-    scanf("%d",&x);
-    loc=search(x,a,sizeof(a)/sizeof(a[0]));
-    if(loc!=-1){
-        printf("%d is on %d order\n",x,loc+1);
-    }else{
-        printf("%d not exit\n",x);
+    for(i=0;i<number;i++){
+        printf("%d",prime[i]);
+        if((i+1)%5)printf("\t");
+        else printf("\n");
     }
     return 0;
     
+    
+//    int x;
+//    scanf("%d",&x);
+//    if(isPrime(x)){
+//        printf("%d is a prime.\n",x);
+//    }else{
+//        printf("%d is not a prime.\n",x);
+//    }
+//    return 0;
 }
-int search(int key,int a[],int length){
-    int ret=-1;
+int isPrime(int x,int knownPrimes[],int numberOfKnownPrimes){
+    int ret=1;
     int i;
-    for(i=0;i<length;i++){
-        if(a[i]==key){
-            ret =i;
+    for( i=0; i<numberOfKnownPrimes; i++ ){
+        if(x % knownPrimes[i]==0){
+            ret = 0;
             break;
         }
     }
