c2f80683372df2814a2783e0729dafa93fe9d860
Haru <happyharu@163.com>
2022-10-05 20:48:32 +0800

    取地址运算
    //变量的地址
    //相邻变量的地址
    //&的结果的sizeof

--- hello/hello/main.c
+++ hello/hello/main.c
@@ -4,83 +4,18 @@
 //
 //  Created by Haru on 2022/9/6.
 //
-//数组例子：构造范围内的素数表
+//取地址运算
 #include <stdio.h>
-int main(){
-    const int size=3;
-    int board[size][size];
-    int i,j;
-    int numOfX;
-    int numOfO;
-    int result=-1;//-1:没输赢，1:X赢，0:O赢
-    
-    //读入二维数组
-    for(i=0;i<size;i++){
-        for(j=0;j<size;j++){
-            scanf("%d",&board[i][j]);
-        }
-    }
-    
-    //判断行
-    for(i=0;i<size && result==-1;i++){
-        numOfO=numOfX=0;
-        for(j=0;j<size;j++){
-            if(board[i][j]==1){
-                numOfX++;
-            }else{
-                numOfO++;
-            }
-        }
-        if(numOfO==size){
-            result=0;
-        }else if(numOfX==size){
-            result=1;
-        }
-    }
-    
-    //判断列
-    if(result==-1){
-        for(j=0; j<size && result==-1;j++){
-            numOfO=numOfX=0;
-            for(i=0;i<size;i++){
-                if(board[i][j]==1){
-                    numOfX++;
-                }else{
-                    numOfO++;
-                }
-            }
-            if(numOfO==size){
-                result=0;
-            }else if(numOfX==size){
-                result=1;
-            }
-        }
-    }
-   
-    //判断对角线
-        //正对角线
-    numOfO=numOfX=0;
-    for(i=0;i<size;i++){
-        if(board[i][i]==1){
-            numOfX ++;
-        }else if(board[i][i]==0){
-            numOfO ++;
-        }
-    }
-    if(numOfO==size){
-        result=0;
-    }else{
-        result=1;
-    }
-        //反对角线
-    numOfO=numOfX=0;
-    for(i=0;i<size;i++){
-        if(board[i][size-i-1]==1){
-            numOfX ++;
-        }else{
-            numOfO ++;
-        }
-    }
-    
-    return 0;
+int main(void){
+    int i=0;
+    int p;
+    p=(int)&i;
+    printf("0x%x\n",&p);//0xbfeff298
+    printf("%p\n",&i);//0x7ff7bfeff29c
+    printf("%lu\n",sizeof(int));//4
+    printf("%lu\n",sizeof(&i));//8
 }
+
+//变量的地址
+//相邻变量的地址
+//&的结果的sizeof
