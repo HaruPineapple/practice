72c948e0b4077142d2b1e1efb0cd94b8d63de128
Haru <happyharu@163.com>
2022-10-05 21:54:02 +0800

    指针的使用
    取数组内的最大最小值

--- hello/hello/main.c
+++ hello/hello/main.c
@@ -4,27 +4,27 @@
 //
 //  Created by Haru on 2022/9/6.
 //
-//取地址运算
+//指针的使用
 #include <stdio.h>
 
-void f(int *p);
-void g(int k);
+void minmax(int a[],int len,int *min,int *max);
 
 int main(void){
-    int i=6;
-    printf("&i=%p\n",&i);
-    f(&i);
-    g(i);
-    
-    return 0;
+    int a[]={1,2,3,4,5,6,7,12,33,44,55};
+    int min,max;
+    minmax(a,sizeof(a)/sizeof(a[0]),&min,&max);
+    printf("min=%d,max=%d\n",min,max);
 }
 
-void f(int *p){
-    printf(" p=%p\n",p);
-    printf("*p=%d\n",*p);
-    *p=26;
-}
-
-void g(int k){
-    printf(" k=%d\n",k);
+void minmax(int a[],int len,int *min,int *max){
+    int i;
+    *min=*max=a[0];
+    for(i=1;i<len;i++){
+        if(a[i]<*min){
+            *min=a[i];
+        }
+        if(a[i]>*max){
+            *max=a[i];
+        }
+    }
 }
