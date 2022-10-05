1cc90d08f09b39b0d89b436e90ba878fe00b4256
Haru <happyharu@163.com>
2022-10-05 19:47:09 +0800

    二维数组：tic-tac-toe

--- hello/hello/main.c
+++ hello/hello/main.c
@@ -7,31 +7,80 @@
 //数组例子：构造范围内的素数表
 #include <stdio.h>
 int main(){
-    const int maxNumber=25;
-    int isPrime[maxNumber];
-    int i;
-    int x;
+    const int size=3;
+    int board[size][size];
+    int i,j;
+    int numOfX;
+    int numOfO;
+    int result=-1;//-1:没输赢，1:X赢，0:O赢
     
-    //初始化数组
-    for(i=0;i<maxNumber;i++){
-        isPrime[i]=1;
+    //读入二维数组
+    for(i=0;i<size;i++){
+        for(j=0;j<size;j++){
+            scanf("%d",&board[i][j]);
+        }
     }
     
-    //标记范围内的i倍数为合数
-    for(x=2;x<maxNumber;x++){
-        if(isPrime[x]){
-            for(i=2;i*x<maxNumber;i++){
-                isPrime[i*x]=0;
+    //判断行
+    for(i=0;i<size && result==-1;i++){
+        numOfO=numOfX=0;
+        for(j=0;j<size;j++){
+            if(board[i][j]==1){
+                numOfX++;
+            }else{
+                numOfO++;
+            }
+        }
+        if(numOfO==size){
+            result=0;
+        }else if(numOfX==size){
+            result=1;
+        }
+    }
+    
+    //判断列
+    if(result==-1){
+        for(j=0; j<size && result==-1;j++){
+            numOfO=numOfX=0;
+            for(i=0;i<size;i++){
+                if(board[i][j]==1){
+                    numOfX++;
+                }else{
+                    numOfO++;
+                }
+            }
+            if(numOfO==size){
+                result=0;
+            }else if(numOfX==size){
+                result=1;
             }
         }
     }
-    
-    //输出范围内质数
-    for(i=2;i<maxNumber;i++){
-        if(isPrime[i]){
-            printf("%d\t",i);
+   
+    //判断对角线
+        //正对角线
+    numOfO=numOfX=0;
+    for(i=0;i<size;i++){
+        if(board[i][i]==1){
+            numOfX ++;
+        }else if(board[i][i]==0){
+            numOfO ++;
+        }
+    }
+    if(numOfO==size){
+        result=0;
+    }else{
+        result=1;
+    }
+        //反对角线
+    numOfO=numOfX=0;
+    for(i=0;i<size;i++){
+        if(board[i][size-i-1]==1){
+            numOfX ++;
+        }else{
+            numOfO ++;
         }
     }
     
-    printf("\n");
+    return 0;
 }
